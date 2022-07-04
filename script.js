126edf00e7ede942d4f399d9f7e3e21c179551d5
vincaf <vincenzocafassog@gmail.com>
2022-07-04 16:45:38 +0200

    add an image taken from the data js to the page

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const app = new Vue(
         el: '#root',
         data: {
             userName: "",
+            imgSource: "https://play-lh.googleusercontent.com/k5wuPqByH7rTHHDqbdQp2gF-WxrvshgMouN3l1ujfUkMbmOM03o773wvTkK8Jg3ylv8",
         },
     }
 );
